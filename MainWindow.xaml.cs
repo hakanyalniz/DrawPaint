f019b6f3b3b8ff2eb3f749b618ce68b7d7e287bd
hakanyalniz <hakan_yalniz@live.com>
2026-01-28 17:57:19 +0300

    Added comments for clarity.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,13 +32,17 @@ public partial class MainWindow : Window
     // Mouse events
     private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
     {
+        // This is required for later on MouseMove
         isDrawing = true;
         Point startPoint = e.GetPosition(DrawingCanvas);
 
+        // After getting the position of mouse, we create a new Polyline and set brush properties
         currentStroke = new Polyline();
         currentStroke.Stroke = Brushes.Black;
         currentStroke.StrokeThickness = 2;
 
+        // We add a point on the mouse point, then add the current polyline object to the canvas
+        // From now on, when we make changes to it, it will reflect on the canvas until we remove the reference for it
         currentStroke.Points.Add(startPoint);
         DrawingCanvas.Children.Add(currentStroke);
 
@@ -48,9 +52,10 @@ public partial class MainWindow : Window
 
     private void Canvas_MouseMove(object sender, MouseEventArgs e)
     {
-        if (!isDrawing || currentStroke == null)
+        if (!isDrawing || currentStroke == null) // If we are not drawing, return
             return;
 
+        // Get position, add a point to the current mouse position
         Point position = e.GetPosition(DrawingCanvas);
         currentStroke.Points.Add(position);
 
@@ -60,6 +65,7 @@ public partial class MainWindow : Window
 
     private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
     {
+        // We are not drawing and remove the reference
         isDrawing = false;
         currentStroke = null;
 
